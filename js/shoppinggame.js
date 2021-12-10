8abfaec3367217b1aef527d3116c9bd0c442710d
Reese Kling <reese.kling@Reese-Klings-MacBook-Pro.local>
2021-12-10 14:03:36 -0600

    Fixes to getDetails function add.

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -40,13 +40,15 @@ const dateDiff = (date1, date2) => {
 // Here, use Object.defineProperty to create property - daysToExpire
 
 Object.defineProperty(Product.prototype, 'daysToExpire', {
-    get: function () { return dateDiff(this.expiryDate, new Date()) }
+    get: function () {
+        return dateDiff(this.expiryDate, new Date());
+    }
 });
 
 // Add method getDetails to Product here
-Object.defineProperty(Product.prototype, 'getDetails', {
-    getDetails: function () { return `Product Name: ${this.name} , Product Price: ${this.price}` }
-})
+Product.prototype.getDetails = function () {
+    return `Product Name: ${this.name} , Product Price: ${this.price}`
+}
 
 // Define the MagicProduct class here
 function MagicProduct(id, name, price, expiryDate, points, isBonus) {
